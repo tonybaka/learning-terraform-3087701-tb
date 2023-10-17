ca9d921a2990eb63b57221b0b5187d64bdbded90
tonybaka <bakalenko@theinstitutes.org>
2023-10-17 16:18:11 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -8,8 +8,5 @@ terraform {
 
 provider "aws" {
   region  = "us-east-1"
-  #new line added here
   #profile = "my-terraform-project"
-  access_key = "AKIATS24BIKKLRXUG4NO"
-  secret_key = "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"
 }
